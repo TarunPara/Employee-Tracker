4539ee4484d85fdeca1c374e7cb748a7f2698530
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-11 13:53:26 -0400

    connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,26 @@
--- Create database
-CREATE DATABASE IF NOT EXISTS employee_tracker;
-USE employee_tracker;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
--- Create department table
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
--- Create role table
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
--- Create employee table
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,29 @@
--- Inserting data into the 'department' table
 INSERT INTO department (name)
 VALUES 
-('Manufacturing'), 
-('Admin'), 
-('Compliance'), 
-('Tech Support'), 
-('Executive'), 
-('Client Relations');
+('Production'), 
+('Operations'), 
+('Legal'), 
+('Information Technology'), 
+('Management'), 
+('Sales');
 
--- Inserting data into the 'role' table
 INSERT INTO role (title, salary, department_id)
 VALUES 
-('Mechanical Engineer', 105000, 1), 
-('Financial Analyst', 95000, 2), 
-('Compliance Officer', 95000, 3), 
-('Network Administrator', 72000, 4), 
-('Operations Manager', 125000, 5), 
-('Sales Representative', 65000, 6);
+('Engineer', 100000, 1), 
+('Accountant', 90000, 2), 
+('Lawyer', 90000, 3), 
+('IT Specialist', 70000, 4), 
+('Project Manager', 120000, 5), 
+('Telemarketer', 60000, 6);
 
--- Inserting data into the 'employee' table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Leo', 'Fender', 1, 5),
-('Paul', 'Reed', 2, 5),
-('Les', 'Paul', 3, 5),
-('Orville', 'Gibson', 4, 5),
-('George', 'Fullerton', 5, NULL),
-('Adolph', 'Rickenbacker', 6, 10),
-('Frederick', 'Gretsch', 1, 10),
-('Lloyd', 'Loar', 2, 10),
-('Ted', 'McCarty', 5, NULL);
+('Syd', 'Barrett', 1, 5),
+('Nick', 'Mason', 2, 5),
+('Roger', 'Waters', 3, 5),
+('Richard', 'Wright', 4, 5),
+('David', 'Gilmour', 5, NULL),
+('Robert', 'Plant', 6, 9),
+('Jimmy', 'Page', 1, 9),
+('John', 'Bonham', 2, 9),
+('John Paul', 'Jones', 5, NULL);
